2e7577c322be25ea83eb409e7f4f13ba842ff305
yash-bhootda <yrbhootda@gmail.com>
2023-01-16 15:44:49 +0530

    string

--- Data Structures/String/longest_subs_without_repeat.cpp
+++ Data Structures/String/longest_subs_without_repeat.cpp
@@ -17,9 +17,29 @@
 using namespace std;
 int lengthOfLongestSubstring(string s)
 {
+    map<char, int> m;
+    int curr_max = 0;
+    int gmax = 0;
+    int start = 0;
+    fo(0, s.length())
+    {
+        if (m.find(s[i]) != m.end())
+        {
+            start = max(start, m[s[i]] + 1);
+            cout << start << endl;
+        }
+        m[s[i]] = i;
+        gmax = max(i - start + 1, gmax);
+    }
+    return gmax;
 }
 int main()
 {
-
+    string s;
+    // cout << "Enter the string" << endl;
+    getline(cin, s);
+    int x;
+    x = lengthOfLongestSubstring(s);
+    cout << x;
     return 0;
 }
