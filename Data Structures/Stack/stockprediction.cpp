682da92d882501e6cbe770ea51caf2987f748faa
yash-bhootda <yrbhootda@gmail.com>
2023-05-19 00:41:24 +0530

    stack

--- Data Structures/Stack/stockprediction.cpp
+++ Data Structures/Stack/stockprediction.cpp
@@ -1,30 +1,50 @@
 class StockSpanner {
 public:
-    vi x;
-    si st;
-    int i=-1;
-    
+    stack<int> st;
+    vector<int> v;
     StockSpanner() {
         
     }
     
     int next(int price) {
-        int count=1;
-        i++;
-        int k=i-1;
+        int count =1;
         while(!st.empty() && price>=st.top())
         {
-            count=count+ x[k];
-            k=k-x[k];
+            count=count+v.back();
+            v.pop_back();
             st.pop();
+
         }
-        
-        x.pb(count);
+        v.push_back(count);
         st.push(price);
-        return x[i];
-        
-        
-        
+        return count;
+        /*if(st.empty())
+        {
+            st.push({price , 1});
+            return 1;
+        }
+        if (st.top().first > price)
+        {
+            st.push({price,1});
+            return 1;
+        }
+        else
+        {
+            int temp=0;
+            while(st.size()>=1 && st.top().first<=price )
+            {
+                temp=temp+st.top().second;
+                st.pop();
+            }
+            st.push({price , temp+1});
+            return temp+1;
+        }*/
         
     }
 };
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
